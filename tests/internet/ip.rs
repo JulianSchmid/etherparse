01d290d2708ce513856aba4332a9786d4bb2c003
Julian Schmid <info@julianschmid.name>
2018-04-02 19:36:52 +0200

    Added unit tests for ip to complete coverage.

--- tests/internet/ip.rs
+++ tests/internet/ip.rs
@@ -154,34 +154,53 @@ fn ipv4_calc_header_checksum() {
 fn read_ip_header_ipv4() {
     use std::io::Cursor;
 
-    let input = Ipv4Header {
-        header_length: 10,
-        differentiated_services_code_point: 42,
-        explicit_congestion_notification: 3,
-        total_length: 1234,
-        identification: 4321,
-        dont_fragment: true,
-        more_fragments: false,
-        fragments_offset: 4367,
-        time_to_live: 8,
-        protocol: 1,
-        header_checksum: 2345,
-        source: [192, 168, 1, 1],
-        destination: [212, 10, 11, 123]
-    };
-    //serialize
-    let mut buffer: Vec<u8> = Vec::with_capacity(20);
-    buffer.write_ipv4_header_raw(&input, &[]).unwrap();
-    assert_eq!(20, buffer.len());
+    let inputs = [
+        Ipv4Header {
+            header_length: 10,
+            differentiated_services_code_point: 42,
+            explicit_congestion_notification: 3,
+            total_length: 1234,
+            identification: 4321,
+            dont_fragment: true,
+            more_fragments: false,
+            fragments_offset: 4367,
+            time_to_live: 8,
+            protocol: 1,
+            header_checksum: 2345,
+            source: [192, 168, 1, 1],
+            destination: [212, 10, 11, 123]
+        },
+        Ipv4Header {
+            header_length: 10,
+            differentiated_services_code_point: 42,
+            explicit_congestion_notification: 3,
+            total_length: 1234,
+            identification: 4321,
+            dont_fragment: false,
+            more_fragments: true,
+            fragments_offset: 4367,
+            time_to_live: 8,
+            protocol: 1,
+            header_checksum: 2345,
+            source: [192, 168, 1, 1],
+            destination: [212, 10, 11, 123]
+        }
+    ];
+    for input in &inputs {
+        //serialize
+        let mut buffer: Vec<u8> = Vec::with_capacity(20);
+        buffer.write_ipv4_header_raw(&input, &[]).unwrap();
+        assert_eq!(20, buffer.len());
 
-    //deserialize
-    let mut cursor = Cursor::new(&buffer);
-    let result = cursor.read_ip_header().unwrap();
-    assert_eq!(20, cursor.position());
+        //deserialize
+        let mut cursor = Cursor::new(&buffer);
+        let result = cursor.read_ip_header().unwrap();
+        assert_eq!(20, cursor.position());
 
-    match result {
-        IpHeader::Version4(result) => assert_eq!(input, result),
-        value => assert!(false, format!("Expected IpHeaderV4 but received {:?}", value))
+        match result {
+            IpHeader::Version4(result) => assert_eq!(*input, result),
+            value => assert!(false, format!("Expected IpHeaderV4 but received {:?}", value))
+        }
     }
 }
 
@@ -212,6 +231,36 @@ fn read_ip_header_ipv6() {
     match result {
         IpHeader::Version6(result) => assert_eq!(input, result),
         value => assert!(false, format!("Expected IpHeaderV6 but received {:?}", value))
+    }
+}
+
+#[test]
+fn read_ip_header_error() {
+    use std::io::Cursor;
+    let input = Ipv6Header {
+        traffic_class: 1,
+        flow_label: 0x81806,
+        payload_length: 0x8021,
+        next_header: 30,
+        hop_limit: 40,
+        source: [1, 2, 3, 4, 5, 6, 7, 8,
+                 9,10,11,12,13,14,15,16],
+        destination: [21,22,23,24,25,26,27,28,
+                      29,30,31,32,33,34,35,36]
+    };
+    //serialize
+    let mut buffer: Vec<u8> = Vec::with_capacity(20);
+    buffer.write_ipv6_header(&input).unwrap();
+    assert_eq!(40, buffer.len());
+
+    //corrupt the version
+    buffer[0] = 0xff;
+
+    //deserialize
+    let mut cursor = Cursor::new(&buffer);
+    match cursor.read_ip_header() {
+        Err(ReadError::IpUnsupportedVersion(0xf)) => {}, //all good
+        value => assert!(false, format!("Expected a IpUnsupportedVersion error but received {:?}", value))
     }
 }
 
