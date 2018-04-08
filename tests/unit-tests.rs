d9ee6c386a5fa188391e21dbae00e0e552b5e48d
Julian Schmid <info@julianschmid.name>
2018-04-08 15:26:51 +0200

    Added additional unit test for debug serialize.

--- tests/unit-tests.rs
+++ tests/unit-tests.rs
@@ -12,8 +12,57 @@ mod transport;
 
 #[test]
 fn test_debug_write() {
-    println!("{:?}", ErrorField::Ipv4HeaderLength);
-    println!("{:?}", ValueError::Ipv4OptionsLengthBad(13));
-    println!("{:?}", WriteError::ValueError(ValueError::Ipv4OptionsLengthBad(13)));
-    println!("{:?}", ReadError::IpUnsupportedVersion(0));
+    //read error
+    {
+        use ReadError::*;
+        for value in [
+            IoError(std::io::Error::new(std::io::ErrorKind::Other, "oh no!")),
+            IpUnsupportedVersion(0),
+            Ipv4UnexpectedVersion(0),
+            Ipv6UnexpectedVersion(0),
+            Ipv6TooManyHeaderExtensions
+        ].iter() {
+            println!("{:?}", value);
+        }
+    }
+    //write error
+    {
+        use ValueError::Ipv4OptionsLengthBad;
+        use WriteError::*;
+        for value in [
+            IoError(std::io::Error::new(std::io::ErrorKind::Other, "oh no!")),
+            ValueError(Ipv4OptionsLengthBad(0))
+        ].iter() {
+            println!("{:?}", value);
+        }
+    }
+    //value error
+    {
+        use ValueError::*;
+        for value in [
+            Ipv4OptionsLengthBad(0),
+            Ipv4PayloadAndOptionsLengthTooLarge(0),
+            UdpPayloadLengthTooLarge(0),
+            U8TooLarge{value: 0, max: 0, field: ErrorField::Ipv4Ecn},
+            U16TooLarge{value: 0, max: 0, field: ErrorField::Ipv4Ecn},
+            U32TooLarge{value: 0, max: 0, field: ErrorField::Ipv4Ecn}
+        ].iter() {
+            println!("{:?}", value);
+        }
+    }
+    //
+    {
+        use ErrorField::*;
+        for value in [
+            Ipv4HeaderLength,
+            Ipv4Dscp,
+            Ipv4Ecn,
+            Ipv4FragmentsOffset,
+            Ipv6FlowLabel,
+            VlanTagPriorityCodePoint,
+            VlanTagVlanId
+        ].iter() {
+            println!("{:?}", value);
+        }
+    }
 }
