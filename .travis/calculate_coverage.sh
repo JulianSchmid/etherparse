ac5e8489cc4163e28882e05309748ad16a05a129
Julian Schmid <info@julianschmid.name>
2018-08-31 07:05:09 +0200

    Updated kvcov to version 36

--- .travis/calculate_coverage.sh
+++ .travis/calculate_coverage.sh
@@ -5,16 +5,16 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo "Skipping code coverage calculation on OS X."
 else
     # On Linux build instrumented for coverage and run the tests
-    wget https://github.com/SimonKagstrom/kcov/archive/v35.tar.gz &&
-    tar xzf v35.tar.gz &&
-    cd kcov-35 &&
+    wget https://github.com/SimonKagstrom/kcov/archive/v36.tar.gz &&
+    tar xzf v36.tar.gz &&
+    cd kcov-36 &&
     mkdir build &&
     cd build &&
     cmake .. &&
     make &&
     make install DESTDIR=../../kcov-build &&
     cd ../.. &&
-    rm -rf kcov-master &&
+    rm -rf kcov-36 &&
     for file in target/debug/{unit_tests,etherparse}-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
     bash <(curl -s https://codecov.io/bash) &&
     echo "Uploaded code coverage"
