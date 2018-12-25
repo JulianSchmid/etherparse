8f0978a0e096d3e09ff4a239c3e74b7f56213f63
Julian Schmid <info@julianschmid.name>
2018-12-25 15:55:38 +0100

    Reactivated coverage calculation for stable & beta builds on TravisCI.

--- .travis/calculate_coverage.sh
+++ .travis/calculate_coverage.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# only execute on nightly, on beta & stable the coverage calculation currently
-# fails with an unknown instruction exception (2018/10/28).
-if [[ "$TRAVIS_RUST_VERSION" == nightly ]]; then
-    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-        # Don't calculate the coverage on os x
-        echo "Skipping code coverage calculation on OS X."
-    else
-        # On Linux build instrumented for coverage and run the tests
-        wget https://github.com/SimonKagstrom/kcov/archive/v36.tar.gz &&
-        tar xzf v36.tar.gz &&
-        cd kcov-36 &&
-        mkdir build &&
-        cd build &&
-        cmake .. &&
-        make &&
-        make install DESTDIR=../../kcov-build &&
-        cd ../.. &&
-        rm -rf kcov-36 &&
-        for file in target/debug/unit_tests-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF unittests; done &&
-        for file in target/debug/etherparse-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF whitebox_unitttests; done &&
-        echo "Uploaded code coverage"
-    fi
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # Don't calculate the coverage on os x
+    echo "Skipping code coverage calculation on OS X."
+else
+    # On Linux build instrumented for coverage and run the tests
+    wget https://github.com/SimonKagstrom/kcov/archive/v36.tar.gz &&
+    tar xzf v36.tar.gz &&
+    cd kcov-36 &&
+    mkdir build &&
+    cd build &&
+    cmake .. &&
+    make &&
+    make install DESTDIR=../../kcov-build &&
+    cd ../.. &&
+    rm -rf kcov-36 &&
+    for file in target/debug/unit_tests-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF unittests; done &&
+    for file in target/debug/etherparse-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF whitebox_unitttests; done &&
+    echo "Uploaded code coverage"
 fi
