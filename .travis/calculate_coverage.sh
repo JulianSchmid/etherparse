1c6eb3718057872ce561d2d7acd13e379734156f
Julian Schmid <info@julianschmid.name>
2019-12-08 17:31:05 +0100

    Switched to kcov37 for coverage calculcation

--- .travis/calculate_coverage.sh
+++ .travis/calculate_coverage.sh
@@ -5,16 +5,16 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo "Skipping code coverage calculation on OS X."
 else
     # On Linux build instrumented for coverage and run the tests
-    wget https://github.com/SimonKagstrom/kcov/archive/v36.tar.gz &&
-    tar xzf v36.tar.gz &&
-    cd kcov-36 &&
+    wget https://github.com/SimonKagstrom/kcov/archive/v37.tar.gz &&
+    tar xzf v37.tar.gz &&
+    cd kcov-37 &&
     mkdir build &&
     cd build &&
     cmake .. &&
     make &&
     make install DESTDIR=../../kcov-build &&
     cd ../.. &&
-    rm -rf kcov-36 &&
+    rm -rf kcov-37 &&
     for file in target/debug/unit_tests-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF unittests; done &&
     for file in target/debug/etherparse-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; bash <(curl -s https://codecov.io/bash) -cF whitebox_unitttests; done &&
     echo "Uploaded code coverage"
