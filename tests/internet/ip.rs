15c709d0c1183d142b5f32c0a0960fd0cd0ca461
Julian Schmid <info@julianschmid.name>
2020-08-10 09:15:30 +0200

    Added tests for the ip_number constants

--- tests/internet/ip.rs
+++ tests/internet/ip.rs
@@ -137,31 +137,58 @@ fn read_ip_header_error() {
     );
 }
 
-mod ip_traffic_class {
-    
-    #[test]
-    fn is_ipv6_ext_header_value() {
-        use crate::IpNumber;
-        use crate::ip_number::*;
-        let ext_ids = [
-            IPV6_HOP_BY_HOP,
-            IPV6_ROUTE,
-            IPV6_FRAG,
-            ENCAP_SEC,
-            AUTH,
-            IPV6_DEST_OPTIONS,
-            MOBILITY,
-            HIP,
-            SHIM6 as u8,
-            EXP0 as u8,
-            EXP1 as u8
-        ];
+#[test]
+fn is_ipv6_ext_header_value() {
+    use crate::IpNumber;
+    use crate::ip_number::*;
+    let ext_ids = [
+        IPV6_HOP_BY_HOP,
+        IPV6_ROUTE,
+        IPV6_FRAG,
+        ENCAP_SEC,
+        AUTH,
+        IPV6_DEST_OPTIONS,
+        MOBILITY,
+        HIP,
+        SHIM6 as u8,
+        EXP0 as u8,
+        EXP1 as u8
+    ];
 
-        for i in 0..std::u8::MAX {
-            assert_eq!(
-                ext_ids.contains(&i),
-                IpNumber::is_ipv6_ext_header_value(i)
-            );
-        }
+    for i in 0..std::u8::MAX {
+        assert_eq!(
+            ext_ids.contains(&i),
+            IpNumber::is_ipv6_ext_header_value(i)
+        );
+    }
+}
+
+#[test]
+fn ip_number_eq_check() {
+    use crate::ip_number::*;
+    use crate::IpNumber::*;
+    let pairs = &[
+        (IPV6_HOP_BY_HOP, IPv6HeaderHopByHop),
+        (ICMP, Icmp),
+        (IGMP, Igmp),
+        (GGP, Ggp),
+        (IPV4, IPv4),
+        (STREAM, Stream),
+        (TCP, Tcp),
+        (UDP, Udp),
+        (IPV6, Ipv6),
+        (IPV6_ROUTE, IPv6RouteHeader),
+        (IPV6_FRAG, IPv6FragmentationHeader),
+        (ENCAP_SEC, EncapsulatingSecurityPayload),
+        (AUTH, AuthenticationHeader),
+        (IPV6_DEST_OPTIONS, IPv6DestinationOptions),
+        (MOBILITY, MobilityHeader),
+        (HIP, Hip),
+        (SHIM6, Shim6),
+        (EXP0, ExperimentalAndTesting0),
+        (EXP1, ExperimentalAndTesting1),
+    ];
+    for (raw, enum_value) in pairs {
+        assert_eq!(*raw, *enum_value as u8);
     }
 }
