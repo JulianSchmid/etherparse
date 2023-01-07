760710ad2d7eb0682bd8708e867cd4322201cc66
Julian Schmid <info@julianschmid.name>
2023-01-07 15:09:51 +0100

    Added missing err::LenSource tests

--- etherparse/src/err/len_source.rs
+++ etherparse/src/err/len_source.rs
@@ -13,3 +13,37 @@ pub enum LenSource {
     /// Error occured while decoding a TCP header.
     TcpHeaderLen,
 }
+
+#[cfg(test)]
+mod test {
+    use super::LenSource::*;
+    use std::{
+        cmp::{Ord, Ordering},
+        collections::hash_map::DefaultHasher,
+        hash::{Hash, Hasher},
+    };
+
+    #[test]
+    fn debug() {
+        assert_eq!("Slice", format!("{:?}", Slice));
+    }
+
+    #[test]
+    fn clone_eq_hash_ord() {
+        let layer = Slice;
+        assert_eq!(layer, layer.clone());
+        let hash_a = {
+            let mut hasher = DefaultHasher::new();
+            layer.hash(&mut hasher);
+            hasher.finish()
+        };
+        let hash_b = {
+            let mut hasher = DefaultHasher::new();
+            layer.clone().hash(&mut hasher);
+            hasher.finish()
+        };
+        assert_eq!(hash_a, hash_b);
+        assert_eq!(Ordering::Equal, layer.cmp(&layer));
+        assert_eq!(Some(Ordering::Equal), layer.partial_cmp(&layer));
+    }
+}
