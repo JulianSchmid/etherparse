c116fc82dff27535815380a1d5f73725ff72d58a
Julian Schmid <info@julianschmid.name>
2023-11-12 18:42:25 +0100

    Add err::packet::SliceError in preparation for LaxSlicedPacket

--- etherparse/src/err/packet/mod.rs
+++ etherparse/src/err/packet/mod.rs
@@ -7,5 +7,8 @@ pub use eth_slice_error::*;
 mod ip_slice_error;
 pub use ip_slice_error::*;
 
+mod slice_error;
+pub use slice_error::*;
+
 mod transport_checksum_error;
 pub use transport_checksum_error::*;
