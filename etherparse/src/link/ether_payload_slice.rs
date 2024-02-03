53046655d80b22b2a485385deaf4803000f1cd4d
Julian Schmid <info@julianschmid.name>
2024-02-03 22:42:55 +0100

    Added tests for payload slices

--- etherparse/src/link/ether_payload_slice.rs
+++ etherparse/src/link/ether_payload_slice.rs
@@ -1,11 +1,60 @@
 use crate::*;
 
 /// Payload of an IP packet.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash, PartialOrd, Ord)]
 pub struct EtherPayloadSlice<'a> {
     /// Identifying content of the payload.
     pub ether_type: EtherType,
 
     /// Payload
     pub payload: &'a [u8],
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alloc::format;
+
+    #[test]
+    fn debug() {
+        let s = EtherPayloadSlice {
+            ether_type: EtherType::IPV4,
+            payload: &[],
+        };
+        assert_eq!(
+            format!(
+                "EtherPayloadSlice {{ ether_type: {:?}, payload: {:?} }}",
+                s.ether_type, s.payload
+            ),
+            format!("{:?}", s)
+        );
+    }
+
+    #[test]
+    fn clone_eq_hash_ord() {
+        let s = EtherPayloadSlice {
+            ether_type: EtherType::IPV4,
+            payload: &[],
+        };
+        assert_eq!(s.clone(), s);
+
+        use std::collections::hash_map::DefaultHasher;
+        use std::hash::{Hash, Hasher};
+
+        let a_hash = {
+            let mut hasher = DefaultHasher::new();
+            s.hash(&mut hasher);
+            hasher.finish()
+        };
+        let b_hash = {
+            let mut hasher = DefaultHasher::new();
+            s.clone().hash(&mut hasher);
+            hasher.finish()
+        };
+        assert_eq!(a_hash, b_hash);
+
+        use std::cmp::Ordering;
+        assert_eq!(s.clone().cmp(&s), Ordering::Equal);
+        assert_eq!(s.clone().partial_cmp(&s), Some(Ordering::Equal));
+    }
 }
