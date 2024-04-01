26ecc8a3b988961d0b3a477637b1c3a4df91d41d
Julian Schmid <info@julianschmid.name>
2024-04-01 07:02:19 +0200

    Further iteration on defragmentation implementation

--- etherparse-defrag/src/ip_defrag_pool.rs
+++ etherparse-defrag/src/ip_defrag_pool.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use etherparse::*;
 use std::collections::HashMap;
 
 /// Pool of buffers to reconstruct multiple fragmented IP packets in
@@ -19,11 +20,14 @@ where
     Timestamp: Sized + core::fmt::Debug + Clone,
     CustomChannelId: Sized + core::fmt::Debug + Clone + core::hash::Hash + Eq + PartialEq,
 {
-    /// Currently reconstructing TP streams.
+    /// Currently reconstructing IP packets.
     active: HashMap<IpFragId<CustomChannelId>, (IpDefragBuf, Timestamp)>,
 
-    /// Buffers that have finished receiving data and can be re-used.
-    finished: Vec<IpDefragBuf>,
+    /// Data buffers that have finished receiving data and can be re-used.
+    finished_data_bufs: Vec<Vec<u8>>,
+
+    /// Section buffers that have finished receiving data and can be re-used.
+    finished_section_bufs: Vec<Vec<IpFragRange>>,
 }
 
 impl<Timestamp, CustomChannelId> IpDefragPool<Timestamp, CustomChannelId>
@@ -31,5 +35,111 @@ where
     Timestamp: Sized + core::fmt::Debug + Clone,
     CustomChannelId: Sized + core::fmt::Debug + Clone + core::hash::Hash + Eq + PartialEq,
 {
+    /// Add data from a sliced packet.
+    pub fn process_sliced_packet(&mut self, slice: &SlicedPacket, channel_id: CustomChannelId) -> Result<(), IpDefragError> {
 
+        // extract the fragment related data and skip non-fragmented packets
+        let (frag_id, offset, more_fragments, payload) = match &slice.net {
+            Some(NetSlice::Ipv4(ipv4)) => {
+                let header = ipv4.header();
+                if false == header.is_fragmenting_payload() {
+                    // nothing to defragment here, skip packet
+                    return Ok(());
+                }
+
+                let (outer_vlan_id, inner_vlan_id) = match &slice.vlan {
+                    Some(VlanSlice::SingleVlan(s)) => (Some(s.vlan_identifier()), None),
+                    Some(VlanSlice::DoubleVlan(d)) => (Some(d.outer().vlan_identifier()), Some(d.inner().vlan_identifier())),
+                    None => (None, None)
+                };
+
+                (
+                    IpFragId {
+                        outer_vlan_id,
+                        inner_vlan_id,
+                        ip: IpFragVersionSpecId::Ipv4{
+                            source: header.source(),
+                            destination: header.destination(),
+                            identification: header.identification(),
+                        },
+                        channel_id,
+                    },
+                    header.fragments_offset(),
+                    header.more_fragments(),
+                    ipv4.payload()
+                )
+            }
+            Some(NetSlice::Ipv6(ipv6)) => {
+                // skip unfragmented packets
+                if false == ipv6.is_payload_fragmented() {
+                    // nothing to defragment here, skip packet
+                    return Ok(());
+                }
+
+                // get fragmentation header
+                let frag = {
+                    let mut f = None;
+                    for ext in ipv6.extensions().clone().into_iter() {
+                        use Ipv6ExtensionSlice::*;
+                        if let Fragment(frag_it) = ext {
+                            f = Some(frag_it);
+                            break;
+                        }
+                    }
+                    if let Some(f) = f {
+                        f.to_header()
+                    } else {
+                        // nothing to defragment here, skip packet
+                        return Ok(());
+                    }
+                };
+
+                let (outer_vlan_id, inner_vlan_id) = match &slice.vlan {
+                    Some(VlanSlice::SingleVlan(s)) => (Some(s.vlan_identifier()), None),
+                    Some(VlanSlice::DoubleVlan(d)) => (Some(d.outer().vlan_identifier()), Some(d.inner().vlan_identifier())),
+                    None => (None, None)
+                };
+
+                // calculate frag id
+                (
+                    IpFragId {
+                        outer_vlan_id,
+                        inner_vlan_id,
+                        ip: IpFragVersionSpecId::Ipv6{
+                            source: ipv6.header().source(),
+                            destination: ipv6.header().destination(),
+                            identification: frag.identification,
+                        },
+                        channel_id,
+                    },
+                    frag.fragment_offset,
+                    frag.more_fragments,
+                    ipv6.payload()
+                )
+            }
+            None => {
+                // nothing to defragment here, skip packet
+                return Ok(());
+            }
+        };
+
+        // get the reconstruction buffer
+        use std::collections::hash_map::Entry;
+        match self.active.entry(frag_id) {
+            Entry::Occupied(mut entry) => {
+                let buf = entry.get_mut();
+                
+            }
+            Entry::Vacant(mut entry) => {
+
+            }
+        }
+
+        //header.
+        //slice.
+
+        
+
+        Ok(())
+    }
 }
--- etherparse-defrag/src/ip_frag_id.rs
+++ etherparse-defrag/src/ip_frag_id.rs
@@ -1,5 +1,7 @@
 use etherparse::*;
 
+use crate::IpFragVersionSpecId;
+
 /// Values identifying a fragmented packet.
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub struct IpFragId<CustomChannelId = ()>
@@ -12,22 +14,10 @@ where
     /// Second VLAN id of the fragmented packets.
     pub inner_vlan_id: Option<VlanId>,
 
-    /// IP source & destination address.
-    pub ip_src_dst: IpSrcDst,
-
-    /// Identifier in the IP header for the fragmented packet.
-    pub identifier: u32,
+    /// IP source & destination address & identifaction field.
+    pub ip: IpFragVersionSpecId,
 
     /// Custom user defined channel identifier (can be used to differentiate packet
     /// sources if the normal ethernet packets identifier are not enough).
     pub channel_id: CustomChannelId,
-}
-
-/// Source & destionation IP address.
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub enum IpSrcDst {
-    /// IPv4 source & destination pair.
-    Ipv4([u8; 4], [u8; 4]),
-    /// IPv6 source & destination pair.
-    Ipv6([u8; 16], [u8; 16]),
 }
