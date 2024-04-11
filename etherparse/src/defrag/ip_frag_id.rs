a9e2bf66851cf35b2ef9fced5e110a9c32295a0f
Julian Schmid <info@julianschmid.name>
2024-04-11 08:12:07 +0200

    Further work on defragmentation

--- etherparse/src/defrag/ip_frag_id.rs
+++ etherparse/src/defrag/ip_frag_id.rs
@@ -15,6 +15,9 @@ where
     /// IP source & destination address & identifaction field.
     pub ip: IpFragVersionSpecId,
 
+    /// IP number of the payload.
+    pub payload_ip_number: IpNumber,
+
     /// Custom user defined channel identifier (can be used to differentiate packet
     /// sources if the normal ethernet packets identifier are not enough).
     pub channel_id: CustomChannelId,
